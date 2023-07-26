f11752259ef51497ea614ddd614f9ad172ea9b02
Enick Jhony <enick.jhony@gmail.com>
2023-07-25 23:32:48 -0400

    Aplicanto `styled-components` ao projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./Styles/themes/default"
+
 export default function App() {
   return (
     <>
+    <ThemeProvider theme={defaultTheme}>
       <h1>Enick</h1>
+    </ThemeProvider>
     </>
   )
 }
