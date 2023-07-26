29d5cf17294e7862cffa594eff894d6a1bc6e888
Enick Jhony <enick.jhony@gmail.com>
2023-07-25 23:34:43 -0400

    Erro `GlobalStyle` dentro do `app.tsx`

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from "styled-components"
 import { defaultTheme } from "./Styles/themes/default"
+import { GlobalStyle } from "./Styles/global"
 
 export default function App() {
   return (
     <>
     <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
       <h1>Enick</h1>
     </ThemeProvider>
     </>
