9631dd87e03b5c308bcf48396db96f368e9d0f5c
Enick Jhony <enick.jhony@gmail.com>
2023-07-27 11:04:24 -0400

    Add transactions table to `Transactions`

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,52 @@
-import { Header } from "../../components/Header";
-import { Summary } from "../../components/Summary";
+import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable
+} from './styles'
 
-export function Transactions(){
-  return(
+export function Transactions() {
+  return (
     <div>
       <Header />
       <Summary />
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tr>
+            <td width={'50%'}>Desenvolvimento de site</td>
+            <td>
+              <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+            </td>
+            <td>Venda</td>
+            <td>27/07/2023</td>
+          </tr>
+          <tr>
+            <td width={'50%'}>Desenvolvimento de site</td>
+            <td>
+              <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+            </td>
+            <td>Venda</td>
+            <td>27/07/2023</td>
+          </tr>
+          <tr>
+            <td width={'50%'}>Desenvolvimento de site</td>
+            <td>
+              <PriceHighlight variant="outcome">R$ 12.000,00</PriceHighlight>
+            </td>
+            <td>Venda</td>
+            <td>27/07/2023</td>
+          </tr>
+          <tr>
+            <td width={'50%'}>Desenvolvimento de site</td>
+            <td>
+              <PriceHighlight variant="outcome">R$ 12.000,00</PriceHighlight>
+            </td>
+            <td>Venda</td>
+            <td>27/07/2023</td>
+          </tr>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
