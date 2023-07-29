e860960ffafdbe40bed85ca698217cbc737bb0fc
Enick Jhony <enick.jhony@gmail.com>
2023-07-28 22:04:28 -0400

    Adicionando UseContext do `Transaction` ao projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import { ThemeProvider } from "styled-components"
-import { defaultTheme } from "./Styles/themes/default"
-import { GlobalStyle } from "./Styles/global"
-import { Transactions } from "./pages/Transactions"
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './Styles/themes/default'
+import { GlobalStyle } from './Styles/global'
+import { Transactions } from './pages/Transactions'
+import { TransactionsProvider } from './contexts/TransactionsContext'
 
 export default function App() {
   return (
     <>
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle />
-      <Transactions />
-    </ThemeProvider>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyle />
+        <TransactionsProvider>
+          <Transactions />
+        </TransactionsProvider>
+      </ThemeProvider>
     </>
   )
 }
